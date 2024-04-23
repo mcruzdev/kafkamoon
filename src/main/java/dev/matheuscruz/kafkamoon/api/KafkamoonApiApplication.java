06b9263206568fc177a424cbef923f89322880d9
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-04-23 11:26:19 -0300

    chore: start project from spring initializr template
    Signed-off-by: Matheus Cruz <matheuscruz.dev@gmail.com>

---
+++ src/main/java/dev/matheuscruz/kafkamoon/api/KafkamoonApiApplication.java
@@ -0,0 +1,13 @@
+package dev.matheuscruz.kafkamoon.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkamoonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkamoonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/matheuscruz/kafkamoon/api/KafkamoonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.matheuscruz.kafkamoon.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkamoonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
