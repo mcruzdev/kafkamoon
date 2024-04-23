58fb135feec913c7a758bcf44d5ce4bf44486438
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-04-23 17:30:47 -0300

    docs: add OpenAPI info

---
+++ src/main/java/dev/matheuscruz/kafkamoon/api/infrastructure/openapi/OpenAPIConfig.java
@@ -0,0 +1,23 @@
+package dev.matheuscruz.kafkamoon.api.infrastructure.openapi;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+   @Value("${kafkamoon.docs.url}")
+   private String externalUrl;
+
+   @Bean
+   public OpenAPI groupedOpenApi() {
+      return new OpenAPI().info(
+                  new Info().title("Kafkamoon API").description("Kafka management API for hiring test").version("0.1.0"))
+            .externalDocs(new ExternalDocumentation().url(externalUrl).description("Kafkamoon Documentation"));
+   }
+}
