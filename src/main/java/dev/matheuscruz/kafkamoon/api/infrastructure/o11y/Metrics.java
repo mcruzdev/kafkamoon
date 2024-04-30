8222aa1a9dacf228337f3de0fd68ca62b5066de8
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-04-29 22:40:59 -0300

    docs: add javadoc to Metrics interface

--- src/main/java/dev/matheuscruz/kafkamoon/api/infrastructure/o11y/Metrics.java
+++ src/main/java/dev/matheuscruz/kafkamoon/api/infrastructure/o11y/Metrics.java
@@ -1,5 +1,8 @@
 package dev.matheuscruz.kafkamoon.api.infrastructure.o11y;
 
+/**
+ * {@link Metrics} represent a component responsible for sending metrics.
+ */
 public interface Metrics {
 
   void increment(MetricName metric, Tag... tags);
